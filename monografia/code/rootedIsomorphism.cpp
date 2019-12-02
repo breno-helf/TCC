27f8117fcef1500f677c3d7445400d693bdc033c
breno-helf <breno.helfstein@gmail.com>
2019-12-01 23:40:34 -0300

    poster

--- monografia/code/rootedIsomorphism.cpp
+++ monografia/code/rootedIsomorphism.cpp
@@ -4,10 +4,9 @@ using namespace std;
 typedef long long int ll;
 
 const int MAX = 1e5 + 10;
-const ll  MOD = 1e9 + 7;
+const ll  MOD = 103;
 
 vector<int> adj[2][MAX];
-vector<int> centroids[2];
 int n, h[2][MAX], sz[2][MAX];
 ll x[MAX];
 
@@ -16,7 +15,7 @@ ll randomLL(ll max) {
    ll k = d * (max + 1);
    return k;
 }
- 
+
 void calcHeight(int u, int tree, int p) {
    for (int v : adj[tree][u]) {
       if (v != p) {
@@ -36,28 +35,10 @@ ll hashTree(int u, int tree, int p) {
    return myHash;
 }
 
-void calcCentroids(int u, int tree, int p) {
-   sz[tree][u] = 1;
-   bool is_centroid = true;
-   for (int v : adj[tree][u]) {
-      if (v != p) {
-         calcCentroids(v, tree, u);
-         sz[tree][u] += sz[tree][v];
-         if (sz[tree][v] > n / 2)
-            is_centroid = false;
-      }
-   }   
-   if ((n - sz[tree][u]) > n / 2)
-      is_centroid = false;
-   
-   if (is_centroid)
-      centroids[tree].push_back(u);
-}        
-
 int main() {
    srand(time(NULL));
    for (int i = 0; i < MAX; i++)
-      x[i] = randomLL(MOD);
+      x[i] = randomLL(15);
    memset(h, 0, sizeof(h));
 
    int cases = 0;
@@ -69,39 +50,33 @@ int main() {
          for (int i = 1; i < n; i++) {
             int u, v;
             scanf("%d %d", &u, &v);
-            u--; v--;
             adj[t][u].push_back(v);
             adj[t][v].push_back(u);
          }
       }
 
       bool isomorphic = false;
-      calcCentroids(0, 0, -1);
-      calcCentroids(0, 1, -1);      
-      
-      for (int c1 : centroids[0]) {
-         for (int c2 : centroids[1]) {
-            memset(h, 0, sizeof(h));
-            calcHeight(c1, 0, -1);
-            calcHeight(c2, 1, -1);
+
+      memset(h, 0, sizeof(h));
+      calcHeight(1, 0, -1);
+      calcHeight(1, 1, -1);
             
-            ll hash1 = hashTree(c1, 0, -1);
-            ll hash2 = hashTree(c2, 1, -1);
-            if (hash1 == hash2) {
-               isomorphic = true;
-               break;
-            }
-         }
-         if (isomorphic) break;
+      ll hash1 = hashTree(1, 0, -1);
+      ll hash2 = hashTree(1, 1, -1);
+      if (hash1 == hash2) {
+         isomorphic = true;
       }
       
-      if (isomorphic && centroids[0].size() == centroids[1].size())
+      if (isomorphic)
          puts("YES");
       else
          puts("NO");
 
-      for (int t = 0; t < 2; t++) {
-         centroids[t].clear();
+      printf("Hash --> %lld %lld\n", hash1, hash2);
+      printf("maxHeight %d\n", h[0][1] > h[1][1] ? h[0][1] : h[1][1]);
+      printf("x --> %lld %lld %lld %lld %lld %lld\n", x[0], x[1], x[2], x[3], x[4], x[5]);
+      
+      for (int t = 0; t < 2; t++) {         
          for (int i = 0; i <= n; i++) {
             adj[t][i].clear();
             h[t][i] = 0;
